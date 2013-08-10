c1a8349155fd175db2dd1b14aeeef3683e8a9172
Charles Hathaway <chathaway@logrit.com>
2013-08-09 23:07:48 -0400

    Added several templates

---
+++ cms_bootstrap_templates/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('cms.urls')),
+    url(r'^tinymce/', include('tinymce.urls')),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
